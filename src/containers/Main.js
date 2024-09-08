d0b2e63b438db79ef2c5614c1909189c08743d30
costamagno <tommycostamagno@gmail.com>
2024-09-08 11:02:39 +0200

    modification

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
-import Education from "../pages/education/EducationComponent";
 import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
-import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
 
 export default class Main extends Component {
